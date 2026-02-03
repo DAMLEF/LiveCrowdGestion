9cc658653a737e2b68483571a2feb68a69e5a1ca
Damien L <damien.lefeuvre@eleves.ec-nantes.fr>
2026-02-03 13:04:45 +0100

    Ajout du Main - Base de fenêtre Pygame

---
+++ main.py
@@ -0,0 +1,18 @@
+# Bibliothèques
+import pygame
+import sys
+
+# ----------- #
+
+SIZE = (1280, 720)
+
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("LiveCrowdGestion")
+
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
